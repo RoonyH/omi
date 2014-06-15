057738bf251236986fa98b0cc5acd8749b8b0271
Aruna Herath <arunabherath@gmail.com>
2014-06-15 20:31:09 +0530

    Added functions to create a card and a player and give a card to a player.

--- public/javascripts/models/game.js
+++ public/javascripts/models/game.js
@@ -4,6 +4,29 @@ var Game = Backbone.Model.extend({
       type: 'omi',
       players: new PlayerCollection()
     };
+  },
+
+  getPlayerById: function(id){
+  },
+
+  createCard: function(card){
+    var card = new Card(card);
+    return card;
+  }
+
+  createPlayer: function(player){
+    var player = new Player(player);
+    var pv = new PlayerView({model: player});
+    this.get('players').add(player);
+    return player;
+  },
+
+  giveCardToPlayer: function(card, player){
+    this.get('players').get(player).giveCard(card);
+    return card;
+  }
+
+  placeCardOnTable: function(){
   }
 });
 
---
+++ public/javascripts/views/game.js
@@ -0,0 +1,7 @@
+var GameView = Backbone.View.extend({
+  template: $('#game').html(),
+
+  render: function(){
+  },
+
+});
